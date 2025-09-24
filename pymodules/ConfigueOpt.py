9453cffd49bb6b8f60aa37df87d06f593db03e75
Salehi Nasab <ssalehinasab@sbbindplp004>
2025-09-24 12:54:46 -0500

    add pymodules

---
+++ pymodules/ConfigueOpt.py
@@ -0,0 +1,38 @@
+from Mygen import GEMOptions
+
+class ConfOpt:
+    def __init__(self, **kwargs):
+        self._opt = GEMOptions()  # private internal config object
+        self.set(**kwargs)
+
+    def set(self, **kwargs):
+        for k, v in kwargs.items():
+            if hasattr(self._opt, k):
+                setattr(self._opt, k, v)
+            else:
+                raise ValueError(f"Invalid GEMOptions field: '{k}'")
+
+        if self._opt.threads > self._opt.num_chunks:
+            raise ValueError(f"Threads {self._opt.threads} cannot be greater than num_chunks {self._opt.num_chunks}")
+
+    # def __getattr__(self, name):
+    #     # only called if attribute not found in ConfOpt
+    #     return getattr(self._opt, name)
+
+    def get(self):
+        return self._opt
+    
+    @property
+    def chunk_size(self):
+        return self._opt.num_chunks
+    
+    @property
+    def threads(self):
+        return self._opt.threads
+        
+    @property
+    def stream_snps(self):
+        return self._opt.stream_snps
+
+    def __repr__(self):
+        return f"<GEMConfig with threads={self._opt.threads}, pheno_file={self._opt.pheno_file}>"
---
+++ pymodules/__init__.py
@@ -0,0 +1,1 @@
+from .ConfigueOpt import ConfOpt 
